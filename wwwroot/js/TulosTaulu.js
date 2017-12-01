831d2d31782e8ff28d2f7c1a3badd317ab6a51c3
sampoharjula <sampo.harjula@student.lamk.fi>
2017-12-01 11:18:33 +0200

    Formit kuntoon

--- wwwroot/js/TulosTaulu.js
+++ wwwroot/js/TulosTaulu.js
@@ -157,6 +157,30 @@ function changeInputValue(target, method) {
 }
 
 $(document).ready(function () {
-    $(".dropdown-button").dropdown();
     $('[data-toggle="tooltip"]').tooltip();
+
+    //Sidebar
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    // when opening the sidebar
+    $('#sidebarCollapse').on('click', function () {
+        // open sidebar
+        $('#sidebar').addClass('active');
+        // fade in the overlay
+        $('.overlay').fadeIn();
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+
+    // if dismiss or overlay was clicked
+    $('#dismiss, .overlay').on('click', function () {
+        // hide the sidebar
+        $('#sidebar').removeClass('active');
+        // fade out the overlay
+        $('.overlay').fadeOut();
+    });
+
 });
