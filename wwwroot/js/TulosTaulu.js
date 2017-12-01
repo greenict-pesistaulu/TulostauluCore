b9bdf3a88c21742369a40602269babbce3299d25
sampoharjula <sampo.harjula@student.lamk.fi>
2017-12-01 11:27:52 +0200

    dropdown typot

--- wwwroot/js/TulosTaulu.js
+++ wwwroot/js/TulosTaulu.js
@@ -164,23 +164,16 @@ $(document).ready(function () {
         theme: "minimal"
     });
 
-    // when opening the sidebar
+    $('#dismiss, .overlay').on('click', function () {
+        $('#sidebar').removeClass('active');
+        $('.overlay').fadeOut();
+    });
+
     $('#sidebarCollapse').on('click', function () {
-        // open sidebar
         $('#sidebar').addClass('active');
-        // fade in the overlay
         $('.overlay').fadeIn();
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
-
-
-    // if dismiss or overlay was clicked
-    $('#dismiss, .overlay').on('click', function () {
-        // hide the sidebar
-        $('#sidebar').removeClass('active');
-        // fade out the overlay
-        $('.overlay').fadeOut();
     });
 
 });
