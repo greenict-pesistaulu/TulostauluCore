917c8dc79140075c76cbb4bacbe6b757a98bfdb9
Joni Juntunen <Joni.M.Juntunen@student.lamk.fi>
2017-12-04 15:19:11 +0200

    Loading last data from db on page refresh

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TulostauluCore.Models;
 
 namespace TulostauluCore.Pages
 {
     public class IndexModel : PageModel
     {
+        TulostauluContext _ctx;
+
+        public Tulostaulu Taulu { get; set;}
+
+        public IndexModel(TulostauluContext ctx)
+        {
+            _ctx = ctx;
+        }
+
         public void OnGet()
         {
-            
+            Taulu = _ctx.Live.Last();
         }
     }
 }
