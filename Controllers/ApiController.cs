015b3f41018e2d2d899ddeb8a11b5dab85997e1f
Sampo Harjula <Sampo.Harjula@student.lamk.fi>
2018-07-25 17:10:08 +0300

    Simple runs fix

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -216,13 +216,15 @@ namespace TulostauluCore.Controllers
                 if (taulu.PeriodInning > 4)
                 {
                     // Score from DB
-                    int AwayScore = _ctx.Score.Where(x => x.GamePeriod == taulu.GamePeriod).Max(x => x.AwayRuns);
-                    int HomeScore = _ctx.Score.Where(x => x.GamePeriod == taulu.GamePeriod).Max(x => x.HomeRuns);
+                    // int AwayScore = _ctx.Score.Where(x => x.GamePeriod == taulu.GamePeriod).Max(x => x.AwayRuns);
+                    // int HomeScore = _ctx.Score.Where(x => x.GamePeriod == taulu.GamePeriod).Max(x => x.HomeRuns);
                     // Score added before SaveChanges()
-                    AwayScore = _ctx.Score.Local.Where(x => x.GamePeriod == taulu.GamePeriod).Max(x => x.AwayRuns);
-                    HomeScore = _ctx.Score.Local.Where(x => x.GamePeriod == taulu.GamePeriod).Max(x => x.HomeRuns);
+                    // AwayScore = _ctx.Score.Local.Where(x => x.GamePeriod == taulu.GamePeriod).Max(x => x.AwayRuns);
+                    // HomeScore = _ctx.Score.Local.Where(x => x.GamePeriod == taulu.GamePeriod).Max(x => x.HomeRuns);
+                    int AwayScore = taulu.AwayRuns;
+                    int HomeScore = taulu.HomeRuns;
 
-                    /* Debug.WriteLine("SCOREAE " + AwayScore + " : " + HomeScore); */
+                    Debug.WriteLine("SCOREAE " + AwayScore + " : " + HomeScore);
                     if (HomeScore != AwayScore)
                     {
                         if (HomeScore > AwayScore)
